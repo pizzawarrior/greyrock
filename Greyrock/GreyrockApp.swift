16e40601ffa4c8451334939d2fdff67ce1d40c0a
pizzawarrior <i.norstad@gmail.com>
2024-09-22 12:30:44 -0700

    new proj

---
+++ Greyrock/GreyrockApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreyrockApp.swift
+//  Greyrock
+//
+//  Created by ian norstad on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GreyrockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreyrockTests/GreyrockTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreyrockTests.swift
+//  GreyrockTests
+//
+//  Created by ian norstad on 9/22/24.
+//
+
+import Testing
+@testable import Greyrock
+
+struct GreyrockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
